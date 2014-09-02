c4217fd1897d7bcfeb478a7f4d3aa229906c15ea
Sergey Volkov <sergey.volkov.kh@yandex.ua>
2014-09-02 17:40:22 +0300

    Created login page without behavior. Added blank chat page

--- routes/index.js
+++ routes/index.js
@@ -2,4 +2,16 @@ module.exports = function(app) {
     app.get('/', function(req, res) {
         res.render('index', {title: "Chat"});
     });
+
+    app.get('/login', function(req, res) {
+        res.render('login');
+    });
+    app.post('/login', function(req, res) {
+        console.log(req.body.login);
+        console.log(req.body.password);
+    });
+
+    app.get('/chat', function(req, res) {
+        res.render('chat', {title: "Chat room"});
+    });
 };
